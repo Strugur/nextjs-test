c4448983c6beeadbb39711b85a2d4668e9def41c
Strugur <dleaaltiusa@gmail.com>
2020-11-27 22:12:42 +0200

    s2

--- server/dist/range.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(req, res, next) {
-    res.header('Content-Range', 'products 0-20/20');
-    next();
-}
-exports.default = default_1;
--- server/src/range.ts
+++
@@ -1,6 +0,0 @@
-import {Request,Response,NextFunction} from 'express';
-
-export default function (req:Request, res:Response ,next:NextFunction) {
-    res.header('Content-Range','products 0-20/20');
-    next();
-}
